63bdb0968a7f075fd99822ec77f6fbcf9f3bd4d6
ausias <a17danvicfer@inspedralbes.cat>
2022-05-06 16:06:19 +0200

    resultados clase y xml

---
+++ app/src/main/java/cat/inspedralbes/projectefutbol/GarHerManGabVicDan/Resultados.java
@@ -0,0 +1,14 @@
+package cat.inspedralbes.projectefutbol.GarHerManGabVicDan;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Resultados  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultados);
+    }
+}
