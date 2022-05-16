081f2a12ff883db048c8eb71ee4684d449a2994b
Danilo <a17danvicfer@inspedralbes.cat>
2022-05-16 13:11:44 +0200

    okhttp terminado falta connectar recycler view

--- app/src/main/java/cat/inspedralbes/projectefutbol/GarHerManGabVicDan/Resultados.java
+++ app/src/main/java/cat/inspedralbes/projectefutbol/GarHerManGabVicDan/Resultados.java
@@ -1,10 +1,12 @@
 package cat.inspedralbes.projectefutbol.GarHerManGabVicDan;
 
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -26,12 +28,12 @@ import okhttp3.ResponseBody;
 
 public class Resultados extends AppCompatActivity {
     final OkHttpClient client = new OkHttpClient();
-    private RecyclerView recyclerView;
+    RecyclerView recyclerView;
     List<Partido> partidos;
     Partido partido;
-    int id_equipo1;
+    String nom_equipo1;
     String resultado;
-    int id_equipo2;
+    String nom_equipo2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,17 +41,13 @@ public class Resultados extends AppCompatActivity {
         setContentView(R.layout.activity_resultados);
         recyclerView = findViewById(R.id.recyclerViewResultados);
         partidos = new ArrayList<>();
+        partido = new Partido();
 
-        try {
-            run();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        consultaTabla();
 
     }
 
-
-    public void run() throws Exception {
+    private void consultaTabla() {
         Request request = new Request.Builder()
                 .url("http://a18gabmantor.alumnes.labs.inspedralbes.cat/api.php/records/RESULTADOS?join=EQUIPOS")
                 .build();
@@ -63,31 +61,52 @@ public class Resultados extends AppCompatActivity {
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                Log.d("Prova", "Entra");
+
                 try (ResponseBody responseBody = response.body()) {
                     if (!response.isSuccessful())
                         throw new IOException("Unexpected code " + response);
-                    String body = responseBody.string();
+
+                    Log.d("Response", "Entra");
+
                     try {
-                        JSONArray jsonArray = new JSONArray(body);
+
+                        JSONObject jsObject = new JSONObject(response.body().string());
+                        JSONArray jsonArray = jsObject.getJSONArray("records");
                         for (int i = 0; i < jsonArray.length(); i++) {
                             JSONObject jsonObject = jsonArray.getJSONObject(i);
-                            id_equipo1 = jsonObject.getInt("EQUIPO_1");
+                            //Guarda resultado del partido
                             resultado = jsonObject.getString("RESULTADO");
-                            id_equipo2 = jsonObject.getInt("EQUIPO_2");
-                            Log.i("Prova", resultado);
+
+                            //Guarda el nombre de los equipos 1 y 2
+                            nom_equipo1 = jsonObject.getString("EQUIPO_1");
+                            JSONObject equipo1obj = new JSONObject(nom_equipo1);
+                            nom_equipo1 = equipo1obj.getString("NOMBRE");
+
+                            nom_equipo2 = jsonObject.getString("EQUIPO_2");
+                            JSONObject equipo2obj = new JSONObject(nom_equipo2);
+                            nom_equipo2 = equipo2obj.getString("NOMBRE");
+
+                            Log.i("Dentro", nom_equipo1 + " " + resultado + " " + nom_equipo2);
                             partido.setResultado(resultado);
+                            partido.setEquipo1(nom_equipo1);
+                            partido.setEquipo2(nom_equipo2);
+                            partidos.add(partido);
 
+                            Log.i("Dentro Objeto", partido.getEquipo1() + " " + partido.getResultado() + " " + partido.getEquipo2());
                         }
-
                     } catch (JSONException e) {
                         e.printStackTrace();
+                        Log.d("Prova", "Falla Json");
                     }
-
-                    System.out.println(responseBody.string());
                 }
             }
         });
+
+
+
+        Log.i("Fuera", nom_equipo1 + " " + resultado + " " + nom_equipo2);
+        Log.i("FueraLista", partido.getEquipo1() + " " + partido.getResultado() + " " + partido.getEquipo2());
+
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         AdapterResultador adapterResultador = new AdapterResultador(partidos);
         recyclerView.setAdapter(adapterResultador);
@@ -96,104 +115,12 @@ public class Resultados extends AppCompatActivity {
 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*
-
-    public void run() throws Exception {
-        Request request = new Request.Builder()
-                .url("http://a18gabmantor.alumnes.labs.inspedralbes.cat/api.php/records/RESULTADOS")
-                .build();
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
-
-            }
-
-            @Override public void onResponse(Call call, Response response) throws IOException {
-                try (ResponseBody responseBody = response.body()) {
-                    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-                    String body = responseBody.string();
-                    partido = new Partido();
-
-                    try{
-                        JSONArray jsonArray=new JSONArray(body);
-                        for(int i=0;i<jsonArray.length();i++)
-                        {
-                            JSONObject jsonObject=jsonArray.getJSONObject(i);
-                            id_equipo1=jsonObject.getInt("EQUIPO_1");
-                            resultado=jsonObject.getString("RESULTADO");
-                            id_equipo2=jsonObject.getInt("EQUIPO_2");
-                            Log.i("Prova", String.valueOf(resultado));
-                            partido.setResultado(resultado);
-
-                        }
-
-                            }
-
-
-
-                     catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-
-
-                }
-
-
-
-
-
-
-    }*/
-
-
-
-  /*  final OkHttpClient client = new OkHttpClient();
-
-    String  urlequips= "http://a18gabmantor.alumnes.labs.inspedralbes.cat/api.php/records/EQUIPOS?filter=ID,bt,"+id_equipo1+","+id_equipo2;
-
-    Request request = new Request.Builder()
-            .url(urlequips)
-            .build();
-                            client.newCall(request).enqueue(new Callback() {
-@Override
-public void onFailure(@NonNull Call call, @NonNull IOException e) {
-        Log.d("Prova", "FALLA");
-        }
-
-@Override
-public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-        ResponseBody respBody = response.body();
-        String body = respBody.string();
-
-        try{
-        JSONArray jsonArray=new JSONArray(body);
-        for(int i=0;i<jsonArray.length();i++) {
-
-        JSONObject jsonObject=jsonArray.getJSONObject(i);
-        String nombre_equipo1=jsonObject.getString("EQUIPO_1");
-        String nombre_equipo2=jsonObject.getString("EQUIPO_2");
-        partido.setEquipo1(nombre_equipo1);
-        partido.setEquipo2(nombre_equipo2);
-        }
+for (int i = 0; i < 10; i++) {
+        partido.setResultado("2-2");
+        partido.setEquipo1("FC Barcelona");
+        partido.setEquipo2("Real Madrid");
         partidos.add(partido);
-        } catch (JSONException e) {
-        e.printStackTrace();
-        }*/
+        }
+*/
+
